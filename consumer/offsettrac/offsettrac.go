2d9e27588a80fa5282accab7f6e182ff4985e499
Maxim Vladimirskiy <horkhe@gmail.com>
2017-03-01 17:12:26 -0800

    Encode sparse acks in offset meta

--- consumer/offsettrac/offsettrac.go
+++ consumer/offsettrac/offsettrac.go
@@ -5,21 +5,51 @@ import (
 
 	"github.com/mailgun/kafka-pixy/consumer"
 	"github.com/mailgun/kafka-pixy/consumer/offsetmgr"
+	"github.com/mailgun/log"
+	"github.com/pkg/errors"
 )
+
+const (
+	base64EncodeMap = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+	maxDelta        = 0xFFF
+)
+
+var (
+	base64DecodeMap [256]byte
+)
+
+func init() {
+	for i := range base64DecodeMap {
+		base64DecodeMap[i] = 0xFF
+	}
+	for i := range base64EncodeMap {
+		base64DecodeMap[base64EncodeMap[i]] = byte(i)
+	}
+}
 
 // T represents an entity that tracks offered and acknowledged messages and
 // maintains offset data for the current state.
 type T struct {
 	offerTimeout time.Duration
 	offset       offsetmgr.Offset
+	ackRanges    []ackRange
+	msgEntries   []msgEntry
 }
 
 // New creates a new offset tracker instance.
 func New(offset offsetmgr.Offset, offerTimeout time.Duration) *T {
-	return &T{
+	ot := T{
 		offerTimeout: offerTimeout,
 		offset:       offset,
 	}
+	var err error
+	ot.ackRanges, err = decodeAckRanges(offset.Val, offset.Meta)
+	if err != nil {
+		ot.ackRanges = nil
+		ot.offset.Meta = ""
+		log.Errorf("failed to decode ack ranges: %v, err=%+v", offset, err)
+	}
+	return &ot
 }
 
 // OnOffered should be called when a message has been offered to a consumer. It
@@ -31,8 +61,14 @@ func (ot *T) OnOffered(msg *consumer.Message) (offsetmgr.Offset, int) {
 // OnAcked should be called when a message has been acknowledged by a consumer.
 // It returns an offset to be submitted and a total number of pending messages.
 func (ot *T) OnAcked(msg *consumer.Message) (offsetmgr.Offset, int) {
-	ot.offset = offsetmgr.Offset{Val: msg.Offset + 1, Meta: ""}
-	return ot.offset, 0
+	msgCount := len(ot.msgEntries)
+	ot.updateAckRanges(msg.Offset)
+	var err error
+	ot.offset.Meta, err = encodeAckRanges(ot.offset.Val, ot.ackRanges)
+	if err != nil {
+		log.Errorf("failed to encode ack ranges: err=%+v", err)
+	}
+	return ot.offset, msgCount
 }
 
 // NextRetry returns a next message to be retried along with the retry attempt
@@ -45,4 +81,135 @@ func (ot *T) NextRetry() (*consumer.Message, int) {
 // worth waiting for, and if so returns a timeout for that wait.
 func (ot *T) ShouldWait4Ack() (bool, time.Duration) {
 	return false, -1
+}
+
+func (ot *T) updateAckRanges(ackedOffset int64) {
+	ackRangesCount := len(ot.ackRanges)
+	if ackedOffset < ot.offset.Val {
+		log.Errorf("ack before committed: %d", ackedOffset)
+		return
+	}
+	if ackedOffset == ot.offset.Val {
+		if ackRangesCount > 0 && ackedOffset == ot.ackRanges[0].from-1 {
+			ot.offset.Val = ot.ackRanges[0].to
+			ot.ackRanges = ot.ackRanges[1:]
+			return
+		}
+		ot.offset.Val += 1
+		return
+	}
+	for i := range ot.ackRanges {
+		if ackedOffset < ot.ackRanges[i].from {
+			if ackedOffset == ot.ackRanges[i].from-1 {
+				ot.ackRanges[i].from -= 1
+				return
+			}
+			ot.ackRanges = append(ot.ackRanges, ackRange{})
+			copy(ot.ackRanges[i+1:], ot.ackRanges[i:ackRangesCount])
+			ot.ackRanges[i] = newAckRange(ackedOffset)
+			return
+		}
+		if ackedOffset < ot.ackRanges[i].to {
+			log.Errorf("duplicate ack: %d", ackedOffset)
+			return
+		}
+		if ackedOffset == ot.ackRanges[i].to {
+			if ackRangesCount > i+1 && ackedOffset == ot.ackRanges[i+1].from-1 {
+				ot.ackRanges[i+1].from = ot.ackRanges[i].from
+				ackRangesCount -= 1
+				copy(ot.ackRanges[i:ackRangesCount], ot.ackRanges[i+1:])
+				ot.ackRanges = ot.ackRanges[:ackRangesCount]
+				return
+			}
+			ot.ackRanges[i].to += 1
+			return
+		}
+	}
+	ot.ackRanges = append(ot.ackRanges, newAckRange(ackedOffset))
+	return
+}
+
+func encodeAckRanges(base int64, ackRanges []ackRange) (string, error) {
+	ackRangesCount := len(ackRanges)
+	if ackRangesCount == 0 {
+		return "", nil
+	}
+	buf := make([]byte, ackRangesCount*4)
+	for i, ar := range ackRanges {
+		if err := ar.encode(base, buf[i*4:]); err != nil {
+			return "", errors.Wrapf(err, "unsupported range: %+v", ar)
+		}
+		base = ar.to
+	}
+	return string(buf), nil
+}
+
+func decodeAckRanges(base int64, encoded string) ([]ackRange, error) {
+	if encoded == "" {
+		return nil, nil
+	}
+	if len(encoded)&0x3 != 0 {
+		return nil, errors.Errorf("too few chars: %d", len(encoded))
+	}
+	ackRanges := make([]ackRange, len(encoded)/4)
+	buf := []byte(encoded)
+	for i := range ackRanges {
+		if err := ackRanges[i].decode(base, buf[i*4:]); err != nil {
+			return nil, errors.Wrapf(err, "bad encoding: %s", encoded)
+		}
+		base = ackRanges[i].to
+	}
+	return ackRanges, nil
+}
+
+func newAckRange(offset int64) ackRange {
+	return ackRange{offset, offset + 1}
+}
+
+func (ar *ackRange) encode(base int64, b []byte) error {
+	fromDlt := ar.from - base
+	if fromDlt > maxDelta {
+		return errors.Errorf("range `from` delta too big: %d", fromDlt)
+	}
+	toDlt := ar.to - ar.from
+	if toDlt > maxDelta {
+		return errors.Errorf("range `to` delta too big: %d", toDlt)
+	}
+
+	val := fromDlt<<12 | toDlt
+	b[0] = base64EncodeMap[val>>18&0x3F]
+	b[1] = base64EncodeMap[val>>12&0x3F]
+	b[2] = base64EncodeMap[val>>6&0x3F]
+	b[3] = base64EncodeMap[val&0x3F]
+	return nil
+}
+
+func (ar *ackRange) decode(base int64, b []byte) error {
+	if len(b) < 4 {
+		return errors.Errorf("too few chars: %d", len(b))
+	}
+	var val int64
+	for i := 0; i < 4; i++ {
+		chr := base64DecodeMap[b[i]]
+		if chr == 0xFF {
+			return errors.Errorf("invalid char: %c", b[i])
+		}
+		val = val<<6 | int64(chr)
+	}
+	ar.from = base + val>>12
+	ar.to = ar.from + val&0xFFF
+	if ar.from >= ar.to || ar.from <= 0 {
+		return errors.Errorf("invalid range: %v", *ar)
+	}
+	return nil
+}
+
+type msgEntry struct {
+	msg     *consumer.Message
+	retryNo int
+	retryAt time.Time
+}
+
+type ackRange struct {
+	from, to int64
 }
---
+++ consumer/offsettrac/offsettrac_test.go
@@ -0,0 +1,178 @@
+package offsettrac
+
+import (
+	"bytes"
+	"strconv"
+	"testing"
+
+	"github.com/mailgun/kafka-pixy/consumer"
+	"github.com/mailgun/kafka-pixy/consumer/offsetmgr"
+	"github.com/pkg/errors"
+	. "gopkg.in/check.v1"
+)
+
+var _ = Suite(&OffsetTrackerSuite{})
+
+type OffsetTrackerSuite struct {
+}
+
+func Test(t *testing.T) {
+	TestingT(t)
+}
+
+// Acknowledged offsets are properly reflected in ackRanges.
+func (s *OffsetTrackerSuite) TestOnAckedRanges(c *C) {
+	ot := New(offsetmgr.Offset{Val: 300}, -1)
+	for i, tc := range []struct {
+		offset            int64
+		ranges            string
+		skipSymmetryCheck bool
+	}{
+		/*  0 */ {offset: 299, ranges: "300:"},
+		/*  1 */ {offset: 300, ranges: "301:"},
+		/*  2 */ {offset: 302, ranges: "301:302-303"},
+		/*  3 */ {offset: 310, ranges: "301:302-303,310-311"},
+		/*  4 */ {offset: 311, ranges: "301:302-303,310-312"},
+		/*  5 */ {offset: 307, ranges: "301:302-303,307-308,310-312"},
+		/*  6 */ {offset: 305, ranges: "301:302-303,305-306,307-308,310-312"},
+		/*  7 */ {offset: 304, ranges: "301:302-303,304-306,307-308,310-312"},
+		/*  8 */ {offset: 305, ranges: "301:302-303,304-306,307-308,310-312"},
+		/*  9 */ {offset: 308, ranges: "301:302-303,304-306,307-309,310-312"},
+		/* 10 */ {offset: 303, ranges: "301:302-306,307-309,310-312"},
+		/* 11 */ {offset: 309, ranges: "301:302-306,307-312"},
+		/* 12 */ {offset: 306, ranges: "301:302-312"},
+		/* 13 */ {offset: 301, ranges: "312:"},
+		/* 14 */ {offset: 398, ranges: "312:398-399"},
+		/* 15 */ {offset: 399, ranges: "312:398-400"},
+		//        delta is greater then 4095, so large deltas cannot be
+		//        represented in the selected encoding algorithm. They must
+		//        never happen in production, but to be thorough we need to
+		//        handle this case somehow, so we just drop all range info.
+		/* 16 */ {offset: 4496, ranges: "312:", skipSymmetryCheck: true},
+		/* 17 */ {offset: 4495, ranges: "312:398-400,4495-4497"},
+	} {
+		// When
+		offset, _ := ot.OnAcked(&consumer.Message{Offset: tc.offset})
+		ot2 := New(offset, -1)
+
+		// Then
+		c.Assert(offsetToStr(offset), Equals, tc.ranges, Commentf("case: %d", i))
+		if tc.skipSymmetryCheck {
+			continue
+		}
+		// Both nil and empty slice ack ranges become nil when going through
+		// the encode/decode circle.
+		if ot.ackRanges == nil || len(ot.ackRanges) == 0 {
+			c.Assert(ot2.ackRanges, IsNil, Commentf("case: %d", i))
+		} else {
+			c.Assert(ot2.ackRanges, DeepEquals, ot.ackRanges, Commentf("case: %d", i))
+		}
+	}
+}
+
+func (s *OffsetTrackerSuite) TestAckRangeEncodeDecode(c *C) {
+	encoded := make([]byte, 4)
+	for i, tc := range []struct {
+		base int64
+		ar   ackRange
+	}{
+		/* 0 */ {0, ackRange{1, 2}},
+		/* 1 */ {100, ackRange{101, 102}},
+		/* 2 */ {10000, ackRange{14095, 14100}},
+		/* 3 */ {10000, ackRange{10001, 14096}},
+	} {
+		var decoded ackRange
+
+		// When
+		err := tc.ar.encode(tc.base, encoded)
+		c.Assert(err, IsNil, Commentf("case: %d", i))
+		decoded.decode(tc.base, encoded)
+
+		// Then
+		c.Assert(decoded, Equals, tc.ar, Commentf("case: %d", i))
+	}
+}
+
+func (s *OffsetTrackerSuite) TestAckRangeEncodeError(c *C) {
+	encoded := make([]byte, 4)
+	for i, tc := range []struct {
+		base int64
+		ar   ackRange
+		err  error
+	}{
+		/* 0 */ {10000, ackRange{14096, 14097},
+			errors.New("range `from` delta too big: 4096")},
+		/* 1 */ {10000, ackRange{10001, 14097},
+			errors.New("range `to` delta too big: 4096")},
+	} {
+		// When
+		err := tc.ar.encode(tc.base, encoded)
+
+		// Then
+		c.Assert(err.Error(), Equals, tc.err.Error(), Commentf("case: %d", i))
+	}
+}
+
+func (s *OffsetTrackerSuite) TestAckRangeDecodeError(c *C) {
+	var ar ackRange
+	for i, tc := range []struct {
+		encoded string
+		err     error
+	}{
+		/* 0 */ {"abc", errors.New("too few chars: 3")},
+		/* 1 */ {"ab@d", errors.New("invalid char: @")},
+		/* 2 */ {"ABAA", errors.New("invalid range: {10001 10001}")},
+	} {
+		// When
+		err := ar.decode(10000, []byte(tc.encoded))
+
+		// Then
+		c.Assert(err.Error(), Equals, tc.err.Error(), Commentf("case: %d", i))
+	}
+}
+
+func (s *OffsetTrackerSuite) TestNewOffsetAdjusted(c *C) {
+	for i, tc := range []struct {
+		initial  offsetmgr.Offset
+		adjusted offsetmgr.Offset
+	}{
+		/* 0 */ {
+			offsetmgr.Offset{1000, ""},
+			offsetmgr.Offset{1000, ""},
+		},
+		/* 1 */ {
+			offsetmgr.Offset{1000, "abra1234+/PS"},
+			offsetmgr.Offset{1000, "abra1234+/PS"},
+		},
+		/* 2 */ {
+			offsetmgr.Offset{1000, "abra1234+/P"},
+			offsetmgr.Offset{1000, ""},
+		},
+		/* 2 */ {
+			offsetmgr.Offset{1000, "abra1234+/@S"},
+			offsetmgr.Offset{1000, ""},
+		},
+	} {
+		// When
+		ot := New(tc.initial, -1)
+
+		// Then
+		c.Assert(ot.offset, Equals, tc.adjusted, Commentf("case: %d", i))
+	}
+}
+
+func offsetToStr(offset offsetmgr.Offset) string {
+	var buf bytes.Buffer
+	buf.WriteString(strconv.FormatInt(offset.Val, 10))
+	buf.WriteString(":")
+	ackRanges, _ := decodeAckRanges(offset.Val, offset.Meta)
+	for i, ar := range ackRanges {
+		if i != 0 {
+			buf.WriteString(",")
+		}
+		buf.WriteString(strconv.FormatInt(ar.from, 10))
+		buf.WriteString("-")
+		buf.WriteString(strconv.FormatInt(ar.to, 10))
+	}
+	return buf.String()
+}
