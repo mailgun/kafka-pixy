f43a67264346e039d53c4b15b05ba536c5dd68a4
Maxim Vladimirskiy <horkhe@gmail.com>
2018-02-05 16:37:16 +0300

    Vendor with dep v0.4.1

--- scripts/vendor.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-dep ensure
-dep prune
-
-# dep prune does poor job so we have to cleanup some unused source files.
-find vendor/github.com/mailgun/holster/ -type f -maxdepth 1 -delete
-find vendor/golang.org/x/text/internal/ -type f -maxdepth 1 -delete
-find vendor/ -name *_test.go -delete
